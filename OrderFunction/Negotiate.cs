715849a88114b52290b3bf19713ee0698134f347
Tanuj Gyan <35070135+tanujgyan@users.noreply.github.com>
2021-11-30 19:32:24 -0500

    added signalr

---
+++ OrderFunction/Negotiate.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+
+namespace OrderFunction
+{
+    public static class Negotiate
+    {
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "ordersdashboard")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+    }
+}
--- OrderFunction/OrderTriggerFunction.cs
+++ OrderFunction/OrderTriggerFunction.cs
@@ -1,25 +1,44 @@
 using System;
 using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using OrderFunction.Services;
 
 namespace OrderFunction
 {
     public static class OrderTriggerFunction
     {
         [FunctionName("OrderTriggerFunction")]
-        public static void Run([CosmosDBTrigger(
+        public static async Task RunAsync([CosmosDBTrigger(
             databaseName: "OrderDB",
             collectionName: "Order",
             ConnectionStringSetting = "CosmosDbConnectionString",
-            LeaseCollectionName = "leases", CreateLeaseCollectionIfNotExists =true)]IReadOnlyList<Document> input, ILogger log)
-        {
-            if (input != null && input.Count > 0)
+            LeaseCollectionName = "leases", CreateLeaseCollectionIfNotExists =true)]IReadOnlyList<Document> input, ILogger log,
+            [SignalR(HubName = "ordersdashboard")] IAsyncCollector<SignalRMessage> signalRMessages)
             {
-                log.LogInformation("Documents modified " + input.Count);
-                log.LogInformation("First document Id " + input[0].Id);
+            try
+            {
+                var viewModel = new DashboardService().GetDashboardViewModel();
+                if (input != null && input.Count > 0)
+                {
+                    log.LogInformation("Documents modified " + input.Count);
+                    log.LogInformation("First document Id " + input[0].Id);
+                    await signalRMessages.AddAsync(new SignalRMessage
+                    {
+                        Target = "target",
+                        Arguments = new[] { JsonSerializer.Serialize(viewModel) }
+                    });
+                }
+               
+            }
+            catch(Exception ex)
+            {
+                log.LogError(ex.Message);
             }
         }
     }
